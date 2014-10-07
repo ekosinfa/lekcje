efea0f5fd3a1cfe0688028ebbdc497262df2a78b
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-10-07 20:34:34 +0200

    Create oceny.php

---
+++ lekcja-2-30092014/oceny.php
@@ -0,0 +1,27 @@
+<?php
+$oceny = array(
+    "0" => array(
+    	'ocena' => 4,
+    	'waga' => 2
+    	),
+    "1" => array(
+    	'ocena' => 2,
+    	'waga' => 1
+    	),
+    "2" => array(
+    	'ocena' => 6,
+    	'waga' => 3
+    	),
+    "3" => array(
+    	'ocena' => 5,
+    	'waga' => 2
+    	),
+    "4" => array(
+    	'ocena' => 4.5,
+    	'waga' => 2
+    	),
+    "5" => array(
+    	'ocena' => 3.5,
+    	'waga' => 3
+    	)
+);
