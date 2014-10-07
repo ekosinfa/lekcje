ab2ae8485b6e34ad91139e55ecd2811c51996191
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-10-07 20:57:05 +0200

    Create zadanie.php

---
+++ lekcja-3-07102014/zadanie.php
@@ -0,0 +1,36 @@
+<?php
+//Poniższe zadanie od Kuby, analogiczne co do tego co robiliśmy na lekcji
+//Jak macie pytania/wątpliwości to piszcie maile (możemy też na skype pogadać)
+
+
+//Wydawnictwo "Komiks" sprzedało we wrześniu 17905 komiksów. Teraz musi zapłacić
+//podatek za nie. Stwórz program pomagający to obliczyć wiedząc, że: podatek od
+//komiksów o bohaterach wynosi 11%, od komiksów inspirowanych anime 8%, a od
+//innych 6%. Poniżej jest tablica ze sprzedanymi komiksami.
+
+$komiksy = array(
+	"1"=>array(
+		'tytul'=>'SuperMaxxx',
+		'egzemplarze_sprzedane'=>'3975',
+		'cena'=>'23.49',
+		'gatunek'=>'bohaterowie'),
+	"2"=>array(
+		'tytul'=>'Chan zdobywca',
+		'egzemplarze_sprzedane'=>'279',
+		'cena'=>'13.56',
+		'gatunek'=>'anime'),
+	"3"=>array(
+		'tytul'=>'Miasto cienia VI',
+		'egzemplarze_sprzedane'=>'10760',
+		'cena'=>'5.55',
+		'gatunek'=>'bohaterowie'),
+	"4"=>array(
+		'tytul'=>'Jak poznałem Alexa',
+		'egzemplarze_sprzedane'=>'2891',
+		'cena'=>'12.44',
+		'gatunek'=>'inne')
+	);
+
+//Pamiętajcie o <?php na początku.
+
+?>
